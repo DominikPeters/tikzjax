46edec3ade48dd8ee75b186472129c5e4e0f5d20
Dominik Peters <3543224+DominikPeters@users.noreply.github.com>
2026-03-06 19:41:56 +0100

    Custom initex.js

---
+++ initex.js
@@ -0,0 +1,49 @@
+#!/usr/bin/env node
+
+const path = require('path');
+process.chdir(path.join(__dirname, 'web2js'));
+
+const fs = require('fs');
+const library = require('./library');
+const { pages } = require('./commonMemory');
+
+const binary = fs.readFileSync('tex.wasm');
+
+const code = new WebAssembly.Module(binary);
+
+const initexMemory = new WebAssembly.Memory({ initial: pages, maximum: pages });
+library.setMemory(initexMemory.buffer);
+library.setInput('\n*latex.ltx\n\\dump\n\n', () => {});
+
+let wasm = new WebAssembly.Instance(code, { library, env: { memory: initexMemory } });
+library.setWasmExports(wasm.exports);
+
+wasm.exports.main();
+
+const memory = new WebAssembly.Memory({ initial: pages, maximum: pages });
+library.setMemory(memory.buffer);
+library.setInput(
+    '\n&latex\n' +
+        '\\documentclass[margin=0pt]{standalone}\n' +
+        '\\def\\pgfsysdriver{pgfsys-ximera.def}\n' +
+        '\\usepackage[svgnames]{xcolor}\n' +
+        '\\usepackage{tikz}\n' +
+        '\\usetikzlibrary{arrows,arrows.meta,positioning,calc,shapes,shapes.geometric,shapes.arrows,fit,decorations.pathmorphing,decorations.markings,matrix,backgrounds,patterns}\n\n' +
+        '\\DeclareGraphicsExtensions{}\n',
+    () => {
+        library.tex_final_end();
+        const buffer = new Uint8Array(memory.buffer);
+        fs.writeFileSync('core.dump', buffer);
+
+        // Save the files used to a json file.
+        let filesystem = library.getUsedFiles();
+        fs.writeFileSync('initex-files.json', JSON.stringify(filesystem, null, '\t'));
+
+        process.exit();
+    }
+);
+
+wasm = new WebAssembly.Instance(code, { library, env: { memory } });
+library.setWasmExports(wasm.exports);
+
+wasm.exports.main();
